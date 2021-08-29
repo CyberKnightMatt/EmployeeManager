e9f67ab0abf71875aed5c090d0e5aea8b899e60d
Matt <matt@stallings.us>
2021-08-29 13:49:37 -0500

    Initial commit

---
+++ src/test/java/us/stallings/employeemanager/EmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package us.stallings.employeemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
