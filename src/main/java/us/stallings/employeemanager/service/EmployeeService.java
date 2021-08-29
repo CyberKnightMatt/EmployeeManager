2f496679b9d661c491e6e51b41883ff8331bae76
Matt <matt@stallings.us>
2021-08-29 14:37:37 -0500

    findAllEmployees, updateEmployee, findEmployeeById, deleteEmployee added to Employee Service

--- src/main/java/us/stallings/employeemanager/service/EmployeeService.java
+++ src/main/java/us/stallings/employeemanager/service/EmployeeService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import us.stallings.employeemanager.model.Employee;
 import us.stallings.employeemanager.repo.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,5 +18,22 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees() {
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found."));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
     }
 }
